31ac17448fe6ed7fb61ceb76c99de6ad17c9e211
Agus <agusmy3@gmail.com>
2021-07-12 07:45:31 +0700

    second commit

--- javascript.js
+++ javascript.js
@@ -1,10 +1,8 @@
 let num_data = 0;
 let arr_history = [];
-
-document.addEventListener("DOMContentLoaded", function () {
- 
+baca();
+document.addEventListener("DOMContentLoaded", function () { 
     const submitForm = document.getElementById("form_todo");
- 
     submitForm.addEventListener("submit", function (event) {
         event.preventDefault();
         tambah();
@@ -17,8 +15,6 @@ function tambah(){
     const keterangan = document.getElementById("keterangan").value;
     const data_todo = [tugas,tanggal,keterangan];
     arr_todo = [];
-
-    baca();
     
     if(num_data > 0 ){    
         arr_todo = arr_history;
@@ -29,6 +25,7 @@ function tambah(){
 
     localforage.setItem('todo', arr_todo).then(function(value) {
         console.log(value);
+        tampil();        
     }).catch(function(err) {
         console.log(err);
     });
@@ -42,3 +39,24 @@ function baca(){
         console.log(err);
     });
 }
+
+function tampil(){
+    localforage.getItem('todo').then(function(value) {
+        const todoList = document.getElementById("baca");
+        todoList.innerHTML = "";
+ 
+        for (let val of value) {
+            let row = document.createElement('tr');
+            row.innerHTML = "<td>" + val[0] + "</td>";
+            row.innerHTML += "<td>" + val[1] + "</td>";
+            row.innerHTML += "<td>" + val[2] + "</td>";
+        
+            todoList.appendChild(row);
+        }
+
+    }).catch(function(err) {
+        console.log(err);
+    });
+}
+
+tampil();
